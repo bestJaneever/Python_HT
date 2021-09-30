19ab688795635515e342ca685aed7e6f2618293b
bestjaneever <eugeniagrebenko@gmail.com>
2021-09-30 23:07:41 +0300

    add home task 4

--- HT_2.py
+++ HT_2.py
@@ -1,40 +1,57 @@
 import random
 
-random_number = random.randint(2, 10) # choose the amount of dicts
-print(random_number)
 
-random_dicts = [] # create list
-for i in range(random_number): # i - an amount of dicts we've  got
-    d = {} # create a dict
-    random_dict_number = random.randint(1,26) # determine range for lowercase letters
-    print(random_dict_number)
-    random_keys = random.sample(range(ord('a'), ord('z')), random_dict_number)  # list of ordered bumbers, 'ord' - ordered number for each letter in Unicode
-    random_keys = [chr(i) for i in random_keys] # using 'chr' - get letter by it's order number
-    for j in range(random_dict_number): # j - an amount of iterations (depends on amount of letters)
-        random_value = random.randint(0,100) # add random values
-        d[random_keys[j]] = random_value # make pairs key-value
-    random_dicts.append(d) # add dicts to the list
-print(random_dicts)
+def get_random_number(min_val, max_val):
+    # choose the amount of dicts
+    return random.randint(min_val, max_val)
 
-dict_with_positions_and_values = {} # create dict, where for each letter will create list with number of dictionary and value
-for i in range(len(random_dicts)): # i - an amount of iterations (depends on the amount of dicts)
-    for k, v in random_dicts[i].items():  # returns key-value for each dict
-        print(k, v, random_dicts[i])
-        if k not in dict_with_positions_and_values:
-            dict_with_positions_and_values[k] = [[i + 1, v]] # we add key and value to the dict in case its absence
+
+def get_random_dicts(rand_number):
+    rand_dicts_list = []
+    for i in range(rand_number):  # i - an amount of dicts we've  got
+        d = {}  # create a dict
+        random_dict_number = get_random_number(1, 26)  # determine range for lowercase letters
+        random_keys = random.sample(range(ord('a'), ord('z')),
+                                    random_dict_number)  # list of ordered bumbers, 'ord' - ordered number for each letter in Unicode
+        random_keys = [chr(i) for i in random_keys]  # using 'chr' - get letter by it's order number
+        for j in range(random_dict_number):  # j - an amount of iterations (depends on amount of letters)
+            random_value = get_random_number(0, 100)  # add random values
+            d[random_keys[j]] = random_value  # make pairs key-value
+        rand_dicts_list.append(d)  # add dicts to the list
+    return rand_dicts_list
+
+
+def get_dict_with_positions_and_values(rand_dicts):
+    dict_with_positions_and_values = {}  # create dict, where for each letter will create list with number of dictionary and value
+    for i in range(len(rand_dicts)):  # i - an amount of iterations (depends on the amount of dicts)
+        for k, v in rand_dicts[i].items():  # returns key-value for each dict
+            if k not in dict_with_positions_and_values:
+                dict_with_positions_and_values[k] = [[i + 1, v]]  # we add key and value to the dict in case its absence
+            else:
+                dict_with_positions_and_values[k].append(
+                    [i + 1, v])  # we add additional list to the list which already exists to choose max value further
+    return dict_with_positions_and_values
+
+
+def get_final_dict(dictionary):
+    final_dict = {}  # create a dict
+    for k, v in dictionary.items():  # make pairs key-value
+        if len(dictionary[
+                   k]) == 1:  # if the letter has only one value - it should be added to the final_dict
+            final_dict[k] = v[0][
+                1]  # we take the key and its value. as we have only one list we need to use position [0] to get it and we choose position [1] because at this position we have our values
         else:
-            dict_with_positions_and_values[k].append([i + 1, v]) # we add additional list to the list which already exists to choose max value further
-print(dict_with_positions_and_values)
+            max_value = v[0]  # variable for max value. at the beginning we choose [0] position
+            for i in v:  # i - list in value
+                if i[1] > max_value[1]:
+                    max_value = i  # in case i > max value, we change max value for i value
+            final_dict[k + '_' + str(max_value[0])] = max_value[
+                1]  # add to the key '_' and number of position and also max value to the final dict
+    return final_dict
 
-final_dict ={} # create a dict
-for k,v in dict_with_positions_and_values.items(): # make pairs key-value
-    if len(dict_with_positions_and_values[k]) == 1: # if the letter has only one value - it should be added to the final_dict
-        final_dict[k] = v[0][1] # we take the key and its value. as we have only one list we need to use position [0] to get it and we choose position [1] because at this position we have our values
-    else:
-        max_value = v[0] # variable for max value. at the beginning we choose [0] position
-        for i in v: # i - list in value
-            if i[1] > max_value[1]:
-                max_value = i # in case i > max value, we change max value for i value
-        final_dict[k + '_' + str(max_value[0])] = max_value[1] # add to the key '_' and number of position and also max value to the final dict
+
+random_number = get_random_number(2, 10)
+random_dicts = get_random_dicts(random_number)
+dict_with_positions_and_values = get_dict_with_positions_and_values(random_dicts)
+final_dict = get_final_dict(dict_with_positions_and_values)
 print(final_dict)
-
--- HT_3.py
+++ HT_3.py
@@ -6,45 +6,78 @@ it iZ misspeLLing here.fix“iZ” with correct “ is ”, but ONLY when it Iz 
 
 'last iz TO calculate nuMber OF Whitespace characteRS in this Tex.caREFULL, not only Spaces, but ALL whitespaces.I got 87.'''
 
-split_list = initial_text.split('\n\n')  # create a list with initial_text paragraphs
-split_list = [i for i in split_list if len(i) > 0] # remove an empty string
-split_list = [i.strip("'") for i in split_list] # remove "'"
-split_list = [i.strip('"') for i in split_list] # remove '"'
-print(split_list)
 
-pre_final_text = [] # create a variable for transform text without last sentence
-last_sentence = [] # create a list for join last words from the sentences
-for j in split_list: # j - each element of the list
-    sentences = j.split('.') # split for separate sentences
-    sentences = [i for i in sentences if len(i) > 0] # remove an empty string
-    paragraph = [] # create a list with separate sentences
-    #print(sentences)
-    for i in sentences: # i - each element of the sentences
-        words = i.split() # split each sentence into words
-        words = [w.lower() for w in words] # lowercase each word
-        #print(words)
-        words[0] = words[0].capitalize() # capitalize first word
-        print(words)
-        last_sentence.append(words[-1]) # add last words of the sentences
-        join_words = ' '.join(words) + '.' # join words to sentences
-        paragraph.append(join_words) # add sentences to paragraphs
-    join_sentences = ' '.join(paragraph) # join sentences to paragraphs
-    pre_final_text.append(join_sentences) # add paragraphs
-print(pre_final_text)
+def get_split_list(string, delimiter):
+    return initial_text.split('\n\n')  # create a list with initial_text paragraphs
 
-last_sentence[0] = last_sentence[0].capitalize() # capitalize first word
-last_sentence_join = ' '.join(last_sentence) + '.' # join last words to sentence
-final_text = '\n\n'.join(pre_final_text) + ' ' + last_sentence_join # create final text, add whitespaces and sentence with last words
-print(final_text)
 
-text = final_text.replace(' iz ', ' is ') # replace
-print(text)
+def remove_empty_element(split_list):
+    split_list = [i for i in split_list if len(i) > 0] # remove an empty string
+    return split_list
 
-whitespaces = 0 # start amount of whitespaces
-for i in text: # i - element in text
-    if i.isspace():
-        whitespaces = whitespaces + 1 # if whitespace is found - add +1
 
+def remove_character(split_list, character):
+    split_list = [i.strip(character) for i in split_list] # remove "'"
+    return split_list
+
+
+def get_main_text_and_last_sentence(split_list):
+    pre_final_text = [] # create a variable for transform text without last sentence
+    last_sentence = [] # create a list for join last words from the sentences
+    for j in split_list: # j - each element of the list
+        sentences = j.split('.') # split for separate sentences
+        sentences = [i for i in sentences if len(i) > 0] # remove an empty string
+        paragraph = [] # create a list with separate sentences
+        for i in sentences: # i - each element of the sentences
+            words = i.split() # split each sentence into words
+            words = [w.lower() for w in words] # lowercase each word
+            words[0] = words[0].capitalize() # capitalize first word
+            last_sentence.append(words[-1]) # add last words of the sentences
+            join_words = ' '.join(words) + '.' # join words to sentences
+            paragraph.append(join_words) # add sentences to paragraphs
+        join_sentences = ' '.join(paragraph) # join sentences to paragraphs
+        pre_final_text.append(join_sentences) # add paragraphs
+
+    return pre_final_text, last_sentence
+
+
+def get_capitalize_first_word(last_sentence):
+    last_sentence[0] = last_sentence[0].capitalize() # capitalize first word
+
+    return last_sentence
+
+
+def get_text_join(lst, ch):
+    text_join = ch.join(lst) # + '.' # join last words to sentence
+    return text_join
+
+
+def get_text_replaced(string, chars_for_replace, chars_replace_with):
+    text = string.replace(chars_for_replace, chars_replace_with) # replace  ' iz ', ' is '
+    return text
+
+
+def get_whitespaces_amount(string):
+    whitespaces = 0 # start amount of whitespaces
+    for i in string: # i - element in text
+        if i.isspace():
+            whitespaces = whitespaces + 1 # if whitespace is found - add +1
+    return whitespaces
+
+
+split_list = get_split_list(initial_text, '\n\n')
+split_list = remove_empty_element(split_list)
+split_list = remove_character(split_list, "'")
+split_list = remove_character(split_list, '"')
+pre_final_text, last_sentence = get_main_text_and_last_sentence(split_list)
+last_sentence = get_capitalize_first_word(last_sentence)
+last_sentence_join = get_text_join(last_sentence, ' ') + '.'
+pre_final_text_join = get_text_join(pre_final_text, '\n\n')
+final_text = pre_final_text_join + ' ' + last_sentence_join
+final_text_replaced = get_text_replaced(final_text, ' iz ', ' is ')
+whitespaces = get_whitespaces_amount(final_text_replaced)
+
+print(final_text_replaced)
 print('I got ' + str(whitespaces) + ' whitespaces' + '.') # an amount of whitespaces
 
 
